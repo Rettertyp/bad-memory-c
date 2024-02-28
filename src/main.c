0b2dc4f22e1b52074a43dcdda268707f0e0cdcfa
Marius Kaufmann <marius@kaufmanns.de>
2024-02-28 11:33:14 +0100

    add nIntervals as input parameter

--- src/main.c
+++ src/main.c
@@ -3,11 +3,17 @@
 #include "debug.h"
 #include "instanceGen.h"
 #include "test.h"
-
-#define N_INTERVALS 1000
+#include <stdlib.h>
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 int main(int argc, char** argv) {
+  if (argc != 2) {
+    printf("Usage: ./badMemAlg <n_intervals>\n");
+    return 1;
+  }
+
+  const uint32_t N_INTERVALS = atoi(argv[1]);
+
   testRunAll(N_INTERVALS);
   testRunAllDepthFirst(N_INTERVALS);
   return 0;
